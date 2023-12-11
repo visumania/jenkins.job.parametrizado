cf70b3fec91d9103fa065df815395a740de70e2a
Adrian Moreno Monterde <amoreno@stemdo.io>
2023-12-11 09:06:46 +0100

    Actualizacion parametrizadoDSLgroovy

--- parametrizadoDSL.groovy
+++ parametrizadoDSL.groovy
@@ -1,40 +1,23 @@
 job('ejemplo2-job-DSL') {
-	description('Job DSL de ejemplo para el curso de Jenkins')
-  	scm {
-      		git('https://github.com/macloujulian/jenkins.job.parametrizado.git', 'main') { node ->
-        		node / gitConfigName('macloujulian')
-        		node / gitConfigEmail('macloujulian@gmail.com')
-      		}
-    	} 
-  	parameters {
-   		stringParam('nombre', defaultValue = 'Julian', description = 'Parametro de cadena para el Job Booleano')
-      		choiceParam('planeta', ['Mercurio', 'Venus', 'Tierrra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
-      		booleanParam('agente', false)
-    	}
-  	triggers {
-    		cron('H/7 * * * *')
-    	}
-  	steps {
-    		shell("bash jobscript.sh")
-    	}
-  	publishers {
-      		mailer('macloujulian@gmail.com', true, true)
-      		slackNotifier {
-		  notifyAborted(true)
-		  notifyEveryFailure(true)
-		  notifyNotBuilt(false)
-		  notifyUnstable(false)
-		  notifyBackToNormal(true)
-		  notifySuccess(false)
-		  notifyRepeatedFailure(false)
-		  startNotification(false)
-		  includeTestSummary(false)
-		  includeCustomMessage(false)
-		  customMessage(null)
-		  sendAs(null)
-		  commitInfoChoice('NONE')
-		  teamDomain(null)
-		  authToken(null)
-        	}
-    	}
+  description('Job DSL de ejemplo para el curso de Jenkins')
+  scm {
+    git('https://github.com/visumania/jenkins.job.parametrizado.git', 'main') { node -> 
+      node / gitConfigName('visumania')
+      node / gitConfigEmail('adrian.moreno.mon@gmail.com')
+    }
+  }
+  parameters {
+    stringParam('nombre', defaultValue = 'Adrián', description = 'Parámetro de cadena para el Job Booleano')
+    choiceParam('planeta', ['Mercurio', 'Venus', 'Tierra', 'Marte', 'Jupiter', 'Saturno', 'Urano', 'Neptuno'])
+    booleanParam('agente', false)
+  }
+  triggers {
+    cron('H/7 * * * *')
+  }
+  steps {
+    shell("bash jobscript.sh")
+  }
+  publishers {
+    mailer('amoreno@stemdo.io', true, true)
+  }
 }
