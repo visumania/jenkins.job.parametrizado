8befb4eed1f7b89b8d936a1a26f98d55a4651deb
Adrian Moreno Monterde <amoreno@stemdo.io>
2023-12-11 09:50:38 +0100

    Corrección sobre numero del job de ejemplo que se creaba

--- parametrizadoDSL2.groovy
+++ parametrizadoDSL2.groovy
@@ -1,4 +1,4 @@
-job('ejemplo2-job-DSL') {
+job('ejemplo3-job-DSL') {
   description('Job DSL de ejemplo para el curso de Jenkins')
   scm {
     git('https://github.com/visumania/jenkins.job.parametrizado.git', 'main') { node -> 
